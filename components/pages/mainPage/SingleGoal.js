5bb7fe02befade523b1c47fe601eb32f8acc8013
vdan <if.dir.dv@gmail.com>
2023-01-05 16:43:41 +0100

    stage

--- components/pages/mainPage/SingleGoal.js
+++ components/pages/mainPage/SingleGoal.js
@@ -1,13 +1,13 @@
-import {StyleSheet, View, Text, Image, TouchableHighlight} from "react-native";
+import {StyleSheet, View, Text, Image, Pressable} from "react-native";
 import basketImage from '../../../assets/basket.png';
 
 export const SingleGoal = ({singleGoal, delGoalHandler}) => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>{singleGoal.text}</Text>
-            <TouchableHighlight onPress={() => delGoalHandler(singleGoal.id)}>
+            <Pressable onPress={() => delGoalHandler(singleGoal.id)}>
                 <Image source={basketImage} style={styles.image}/>
-            </TouchableHighlight>
+            </Pressable>
         </View>
     )
 }
